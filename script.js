75d503b79df5c6d21d0cbcb50cb701b5c1efc2cb
MatthewPCope <matthew.p.cope@gmail.com>
2024-08-07 16:35:40 -0700

    done for now

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const signupForm = document.getElementById("main")
+    const subscribeForm = document.getElementById("main")
     const successMessage = document.getElementById("modal")
     const subscribeBtn = document.getElementById("subscribe-btn")
     const email_alert = document.getElementById("valid-email-alert")
@@ -7,14 +7,18 @@ document.addEventListener('DOMContentLoaded', function() {
     const emailInput = document.getElementById("email_input")
     const emailDisplay = document.querySelector("#modal p strong")
 
+    function validateEmail(text) {
+        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return re.test(text);
+    }
+
     subscribeBtn.addEventListener('click', (e) => {
         e.preventDefault();
         const email = emailInput.value;
 
         if (validateEmail(email)) {
-            console.log(emailDisplay)
             emailDisplay.textContent = email;
-            signupForm.classList.add("hidden");
+            subscribeForm.classList.add("hidden");
             successMessage.classList.remove("hidden");
             email_alert.classList.add("hidden");
             emailInput.classList.remove("alert");
@@ -26,12 +30,8 @@ document.addEventListener('DOMContentLoaded', function() {
 
     dismissBtn.addEventListener('click', () => {
         successMessage.classList.add("hidden");
-        signupForm.classList.remove("hidden");
+        subscribeForm.classList.remove("hidden");
         emailInput.value = '';
     });
 
-    function validateEmail(text) {
-        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return re.test(text);
-    }
 })
