3b1a4d4a94bf42151ceec43a66345a8f2631f0c0
MatthewPCope <matthew.p.cope@gmail.com>
2024-08-07 16:15:34 -0700

    close

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var modal = this.getElementById('modal')
-    var closeBtn = this.getElementsByClassName('close')[0]
+document.addEventListener('DOMContentLoaded', function() {
+    const signupForm = document.getElementById("main")
+    const successMessage = document.getElementById("modal")
+    const subscribeBtn = document.getElementById("subscribe-btn")
+    const email_alert = document.getElementById("valid-email-alert")
+    const dismissBtn = document.getElementById("dismiss-btn")
+    const emailInput = document.getElementById("email_input")
+    const emailDisplay = document.querySelector("#modal p strong")
 
-    document.querySelector("form").addEventListener("submit", function(event) {
-        event.preventDefault();
-        modal.style.display = "block";
+    subscribeBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        const email = emailInput.value;
+
+        if (validateEmail(email)) {
+            console.log(emailDisplay)
+            emailDisplay.textContent = email;
+            signupForm.classList.add("hidden");
+            successMessage.classList.remove("hidden");
+            email_alert.classList.add("hidden");
+            emailInput.classList.remove("alert");
+        } else {
+            email_alert.classList.remove("hidden");
+            emailInput.classList.add("alert");
+        }
     });
 
-    closeBtn.onclick = function() {
-        modal.style.display = "none";
-    };
+    dismissBtn.addEventListener('click', () => {
+        successMessage.classList.add("hidden");
+        signupForm.classList.remove("hidden");
+        emailInput.value = '';
+    });
 
-    window.onclick = function(event) {
-        if (event.target == modal) {
-        
-            modal.style.display = "none";
-        }
-    };
+    function validateEmail(text) {
+        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return re.test(text);
+    }
 })
